96dee40371a4addd6dbdb09f9151b66df285fd88
d74117157-create <d74117157@gmail.com>
2026-07-08 05:47:25 +0000

    Add frontend/next.config.ts

---
+++ frontend/next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  output: 'standalone',
+  async redirects() {
+    return [{ source: '/', destination: '/dashboard', permanent: true }];
+  }
+};
+
+export default nextConfig;
